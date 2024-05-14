f4cc51594b9c97feb5e0513b5fd683c8b4333202
Keivalya Pandya <keivalyapandya2001@gmail.com>
2024-05-14 15:30:38 +0530

    changes done

--- core/settings.py
+++ core/settings.py
@@ -47,7 +47,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "store.views.categories",
+                "store.context_processors.categories",
             ],
         },
     },
